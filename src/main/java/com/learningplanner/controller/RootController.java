fb5e87b1ec01aefe42c9e041b2970c1e536b1e63
Shivamani Bandari <shivamanibandari3@gmail.com>
2026-01-05 14:15:13 +0530

    added rootcontroller

---
+++ src/main/java/com/learningplanner/controller/RootController.java
@@ -0,0 +1,13 @@
+package com.learningplanner.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Smart Learning Scheduler API is running 🚀";
+    }
+}
